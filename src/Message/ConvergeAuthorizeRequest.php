2b0c81659492b0cf1ab31c0471873d7e4da6af59
Konstantin Bahchevanov <k_bahchevanov@hotmail.com>
2016-12-12 12:52:03 +0200

    Update ConvergeAuthorizeRequest.php

--- src/Message/ConvergeAuthorizeRequest.php
+++ src/Message/ConvergeAuthorizeRequest.php
@@ -27,10 +27,7 @@ class ConvergeAuthorizeRequest extends ConvergeAbstractRequest
                 'ssl_country' => $this->getCard()->getCountry(),
                 'ssl_customer_code' => $this->getDescription(),
                 'ssl_avs_zip' => $this->getCard()->getPostcode(),
-                'ssl_invoice_number' => $this->getTransactionId(),
-                'ssl_result_format' => $this->getSslResultFormat(),
-                'ssl_test_mode' => $this->getSslTestMode(),
-                'ssl_show_form' => $this->getSslShowForm()
+                'ssl_invoice_number' => $this->getTransactionId()
             );
         } elseif ($this->getCardReference() != null) {
                 $this->validate('amount');
