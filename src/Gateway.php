d88cd159e36b566b7e45fd9f53d7d348a30b685f
Konstantin Bahchevanov <k_bahchevanov@hotmail.com>
2016-12-12 11:57:23 +0200

    Create Gateway.php
    Create Gateway.php

---
+++ src/Gateway.php
@@ -0,0 +1,115 @@
+<?php namespace Omnipay\Elavon;
+
+use Omnipay\Common\AbstractGateway;
+
+/**
+ * Elavon's Converge Gateway
+ *
+ * @link https://www.myvirtualmerchant.com/VirtualMerchant/
+ */
+class Gateway extends AbstractGateway
+{
+    public function getName()
+    {
+        return 'Converge';
+    }
+
+    public function getDefaultParameters()
+    {
+        return array(
+            'merchantId' => '',
+            'username' => '',
+            'password' => '',
+            'ssl_result_format' => 'ASCII',
+            'ssl_test_mode' => 'true',
+            'ssl_show_form' => 'false'
+        );
+    }
+
+    public function getMerchantId()
+    {
+        return $this->getParameter('merchantId');
+    }
+
+    public function setMerchantId($value)
+    {
+        return $this->setParameter('merchantId', $value);
+    }
+
+    public function getUsername()
+    {
+        return $this->getParameter('username');
+    }
+
+    public function setUsername($value)
+    {
+        return $this->setParameter('username', $value);
+    }
+
+    public function getPassword()
+    {
+        return $this->getParameter('password');
+    }
+
+    public function setPassword($value)
+    {
+        return $this->setParameter('password', $value);
+    }
+
+    public function getSslShowForm()
+    {
+        return $this->getParameter('ssl_show_form');
+    }
+
+    public function setSslShowForm($value)
+    {
+        return $this->setParameter('ssl_show_form', $value);
+    }
+
+    public function getSslResultFormat()
+    {
+        return $this->getParameter('ssl_result_format');
+    }
+
+    public function setSslResultFormat($value)
+    {
+        return $this->setParameter('ssl_result_format', $value);
+    }
+
+    public function getSslTestMode()
+    {
+        return $this->getParameter('ssl_test_mode');
+    }
+
+    public function setSslTestMode($value)
+    {
+        return $this->setParameter('ssl_test_mode', $value);
+    }
+
+    /**
+     * @param array $parameters
+     * @return \Omnipay\Elavon\Message\AuthorizeRequest
+     */
+    public function authorize(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Elavon\Message\ConvergeAuthorizeRequest', $parameters);
+    }
+
+    /**
+     * @param array $parameters
+     * @return \Omnipay\Elavon\Message\PurchaseRequest
+     */
+    public function purchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Elavon\Message\ConvergePurchaseRequest', $parameters);
+    }
+
+    /**
+     * @param array $parameters
+     * @return \Omnipay\Elavon\Message\PurchaseRequest
+     */
+    public function createCard(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Elavon\Message\ConvergeGenerateTokenRequest', $parameters);
+    }
+}
